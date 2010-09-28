ca93f4f6f97b0249682c3291ddc241be56e22760
Adam Langley <agl@chromium.org>
2010-09-28 10:01:40 -0400

    C++ support and constify pointers
    Patch-By: Jack Lloyd

--- ctgrind.c
+++ ctgrind.c
@@ -1,12 +1,12 @@
-#include <sys/types.h>
+#include "ctgrind.h"
 
 // These are no-op functions which exist only so that valgrind can intercept
 // them.
 
-void ct_poison(void *dat, size_t datlen)
+void ct_poison(const void *dat, size_t datlen)
 {
 }
 
-void ct_unpoison(void *dat, size_t datlen)
+void ct_unpoison(const void *dat, size_t datlen)
 {
 }
--- ctgrind.h
+++ ctgrind.h
@@ -1,9 +1,15 @@
 #ifndef CT_GRIND_H_
 #define CT_GRIND_H_
 
-#include <sys/types.h>
+#include <stddef.h>
 
-extern void ct_poison(void* address, size_t length);
-extern void ct_unpoison(void* address, size_t length);
+#if defined(__cplusplus)
+  #define CT_POISON_EXTERN extern "C"
+#else
+  #define CT_POISON_EXTERN extern
+#endif
+
+CT_POISON_EXTERN void ct_poison(const void* address, size_t length);
+CT_POISON_EXTERN void ct_unpoison(const void* address, size_t length);
 
 #endif
